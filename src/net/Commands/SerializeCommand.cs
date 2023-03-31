7bdd270c15730b76e5517e3921dd2cb28e3a647f
Enrique Raso Barbero <ERasoBarbero@omp.com>
2023-03-31 09:57:29 +0200

    Add Union argument in SerializeCommand

--- src/net/Commands/SerializeCommand.cs
+++ src/net/Commands/SerializeCommand.cs
@@ -16,11 +16,14 @@ namespace FlatBufferTest.Commands
         [Option("-o|--output", Description = "Output file with serialization")]
         public string OutputFile { get; }
 
-        [Option("-t|--type", Description = "Type of object to serialize")]
+        [Option("-t|--type", Description = "Type of object to serialize.")]
         public OverallLikelyExceptionUnion Type { get; }
 
         [Option("-v|--value", Description = "A string value for DetailedException2 type and unsigned long value for DetailedException1 type.")]
         public string Value { get; }
+
+        [Option("-u|--union", Description = "Serialize as DetailedException3 type that contains a union of DetailedException2 and DetailedException1.")]
+        public bool Union { get; } = false;
 
         public async Task OnExecuteAsync()
         {
@@ -39,7 +42,7 @@ namespace FlatBufferTest.Commands
                 bytes = GetNone();
             }
 
-            var fileName = $"{OutputFile}.{Type}";
+            var fileName = $"{OutputFile}.{Type}{(Union ? "" : ".simple")}";
             EnsurePathExists(fileName);
 
             await File.WriteAllBytesAsync(fileName, bytes);
@@ -62,9 +65,17 @@ namespace FlatBufferTest.Commands
             var exceptionValueOffset = builder.CreateString(Value);
 
             var detailedException2Offset = DetailedException2.CreateDetailedException2(builder, exceptionValueOffset);
-            var detailedException3Offset = DetailedException3.CreateDetailedException3(builder, OverallLikelyExceptionUnion.DetailedException2, detailedException2Offset.Value);
 
-            builder.Finish(detailedException3Offset.Value);
+            if (Union)
+            {
+                var detailedException3Offset = DetailedException3.CreateDetailedException3(builder, OverallLikelyExceptionUnion.DetailedException2, detailedException2Offset.Value);
+                builder.Finish(detailedException3Offset.Value);
+            }
+            else
+            {
+                builder.Finish(detailedException2Offset.Value);
+            }
+
             return builder.SizedByteArray();
         }
 
@@ -77,14 +88,27 @@ namespace FlatBufferTest.Commands
             }
 
             var detailedException1Offset = DetailedException1.CreateDetailedException1(builder, value);
-            var detailedException3Offset = DetailedException3.CreateDetailedException3(builder, OverallLikelyExceptionUnion.DetailedException1, detailedException1Offset.Value);
 
-            builder.Finish(detailedException3Offset.Value);
+            if (Union)
+            {
+                var detailedException3Offset = DetailedException3.CreateDetailedException3(builder, OverallLikelyExceptionUnion.DetailedException2, detailedException1Offset.Value);
+                builder.Finish(detailedException3Offset.Value);
+            }
+            else
+            {
+                builder.Finish(detailedException1Offset.Value);
+            }
+            
             return builder.SizedByteArray();
         }
 
         private byte[] GetNone()
         {
+            if (!Union)
+            {
+                throw new InvalidOperationException($"Specify union argument to serialize {OverallLikelyExceptionUnion.NONE}.");
+            }
+
             var builder = new FlatBufferBuilder(1);
 
             var detailedException3Offset = DetailedException3.CreateDetailedException3(builder, OverallLikelyExceptionUnion.NONE);
