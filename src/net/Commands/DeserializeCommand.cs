fee48af216a703a29b0eaa0cc6f8d3b967124bc4
Enrique Raso Barbero <ERasoBarbero@omp.com>
2023-03-31 13:08:42 +0200

    Add Union argument in DeserializeCommand

--- src/net/Commands/DeserializeCommand.cs
+++ src/net/Commands/DeserializeCommand.cs
@@ -1,4 +1,5 @@
-﻿using FlatBuffers;
+﻿using System;
+using FlatBuffers;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Logging;
 using OMTLS.FlatBuffers;
@@ -19,14 +20,43 @@ namespace FlatBufferTest.Commands
         [Option("-t|--type", Description = "Type of object to deserialize")]
         public OverallLikelyExceptionUnion Type { get; }
 
+        [Option("-u|--union", Description = "Deserialize as DetailedException3 type that contains a union of DetailedException2 and DetailedException1.")]
+        public bool Union { get; } = false;
+
         public async Task OnExecuteAsync()
         {
             var bytes = await File.ReadAllBytesAsync(InputFile);
             var buffer = new ByteBuffer(bytes);
             
+            if (Union)
+            {
+                DeserializeAsDetailedException3(buffer);
+            }
+            else if (Type == OverallLikelyExceptionUnion.DetailedException2)
+            {
+                var detailedException2 = DetailedException2.GetRootAsDetailedException2(buffer);
+                Logger.LogInformation("_exceptionValue = {Value} ({Type})", detailedException2._exceptionValue, Type);
+            }
+            else if (Type == OverallLikelyExceptionUnion.DetailedException1)
+            {
+                var detailedException1 = new DetailedException1();
+                detailedException1.__assign(8, buffer);
+                
+                Logger.LogInformation("_exceptionValue = {Value} ({Type})", detailedException1._exceptionValue, Type);
+            }
+            else
+            {
+                Logger.LogError("Type not supported.");
+            }
+
+            return;
+        }
+
+        private void DeserializeAsDetailedException3(ByteBuffer buffer)
+        {
             var detailedException3 = DetailedException3.GetRootAsDetailedException3(buffer);
             Logger.LogInformation("DetailedException3 contains type {Type}", detailedException3.AType);
-            
+
             if (detailedException3.AType == OverallLikelyExceptionUnion.DetailedException2)
             {
                 var detailedException2 = detailedException3.A<DetailedException2>().Value;
